a981952a3fe2e4acbb71bd6bbb4798e8b31b6f98
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-07-27 11:01:39 -0400

    Refactor and enhance core application structure
    - Organized project structure in `Core.Application.csproj` by adding folders for mappings and removing the `ViewModels` folder reference.
    - Introduced `IGenericServices<TDto, Tkey>` interface and refactored `GenericServices` to support generic CRUD operations with dependency injection, error handling, and validation.
    - Added `BaseViewModel<TKey>` for consistent view model structure.
    - Centralized error definitions in `GeneralError` for reusable error handling.
    - Added `StatusFilter` enum for status-based filtering.
    - Integrated `EntityFrameworkCore` packages and added folders for configurations, context, and repositories in `Infrastructure.Persistence`.
    - Added `EntityFrameworkCore.Design` package to `Presentation.WebApp` for design-time operations.

--- Source/Core/Artemis Banking Pro.Core.Application/Contracts/Generic/IGenericServices.cs
+++ Source/Core/Artemis Banking Pro.Core.Application/Contracts/Generic/IGenericServices.cs
@@ -1,4 +1,5 @@
 ﻿using ArtemisBankingPro.Core.Domain.Common.ValidationResult;
+using ArtemisBankingPro.Core.Domain.Entities.Base;
 
 namespace Artemis_Banking_Pro.Core.Application.Contracts.Generic
 {
--- Source/Core/Artemis Banking Pro.Core.Application/Services/Generic/GenericServices.cs
+++ Source/Core/Artemis Banking Pro.Core.Application/Services/Generic/GenericServices.cs
@@ -1,8 +1,103 @@
 ﻿using Artemis_Banking_Pro.Core.Application.Contracts.Generic;
+using ArtemisBankingPro.Core.Domain.Common.Errors;
+using ArtemisBankingPro.Core.Domain.Common.ValidationResult;
+using ArtemisBankingPro.Core.Domain.Entities.Base;
+using ArtemisBankingPro.Core.Domain.Interfaces.Generic;
+using ArtemisBankingPro.Core.Domain.CodeErrors.GeneralErrors;
+using AutoMapper;
+
 
 namespace Artemis_Banking_Pro.Core.Application.Services.Generic
 {
-    public abstract class GenericServices 
-   { 
+   public abstract class GenericServices<TDto, Tkey, TEntity>
+       : IGenericServices<TDto, Tkey>
+       where TEntity : BaseEntitie<Tkey>
+       where TDto : class
+   {
+        protected readonly IGenericRepository<TEntity, Tkey> _genericRepository;
+        protected readonly IMapper _mapper;
+        protected readonly List<Error> _errors = new List<Error>();
+
+        public GenericServices(IGenericRepository<TEntity, Tkey> genericRepository, 
+            IMapper mapper,
+            List<Error> errors)
+        {
+            _genericRepository = genericRepository;
+            _mapper = mapper;
+            _errors = errors;
+        }
+
+        public virtual async Task<ValidationResult> CreateAsync(TDto dto)
+        {
+            try
+            {
+                var map = _mapper.Map<TEntity>(dto);
+                await _genericRepository.AddAsync(map);
+                return ValidationResult.Failure();
+            }
+            catch (Exception)
+            {
+                _errors.Add(GeneralError.UnexpectedError);
+                return ValidationResult.Failure(_errors);
+            }
+        }
+
+        public virtual async Task<ValidationResult<IReadOnlyCollection<TDto>>> GetAllAsync(int page, int pageSize)
+        {
+            try
+            {
+                var result = await _genericRepository.GetAllAsync(page, pageSize);
+                var map = _mapper.Map <IReadOnlyCollection<TDto>>(result);
+                return ValidationResult<IReadOnlyCollection<TDto>>.Success(map);
+            }
+            catch (Exception)
+            {
+                _errors.Add(GeneralError.UnexpectedError);
+                return ValidationResult<IReadOnlyCollection<TDto>>.Failure(_errors);
+            }
+        }
+
+        public virtual async Task<ValidationResult<TDto>> GetByIdAsync(Tkey tkey)
+        {
+            try
+            {
+                var entite = await _genericRepository.GetByIdAsync(tkey);
+                if(entite is null)
+                {
+                    _errors.Add(GeneralError.NonExistence);
+                    return ValidationResult<TDto>.Failure(_errors);
+                }
+                var map = _mapper.Map<TDto>(entite);
+                return ValidationResult<TDto>.Success(map);
+
+            } catch (Exception)
+            {
+                _errors.Add(GeneralError.UnexpectedError);
+                return ValidationResult<TDto>.Failure(_errors);
+            }
+        }
+
+        public virtual async Task<ValidationResult> UpdateAsync(TDto dto)
+        {
+            try
+            {
+               var map = _mapper.Map<TEntity>(dto);
+               var  success =  await _genericRepository.UpdateAsync(map);
+                if (!success)
+                {
+                    _errors.Add(GeneralError.UnexpectedError);
+                    return ValidationResult<IReadOnlyCollection<TDto>>.Failure(_errors);
+
+                }
+                return ValidationResult.Success();
+            }
+            catch (Exception)
+            {
+                _errors.Add(GeneralError.UnexpectedError);
+                return ValidationResult<IReadOnlyCollection<TDto>>.Failure(_errors);
+            }
+        }
     }
-}
+
+   }
+
