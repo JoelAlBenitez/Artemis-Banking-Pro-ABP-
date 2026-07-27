6a25eb4af1a595265000a4932fa9f88be6d11446
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-07-27 09:34:56 -0400

    Refactor repository and remove error handling views
    Updated `IGenericRepository` to support generics with `TEntity`
    and `TKey`, adding methods for CRUD operations. Added
    `AutoMapper` package to the project for object mapping.
    
    Removed `ErrorViewModel`, `Error.cshtml`, and the `Error`
    action from `HomeController`, simplifying error handling.
    Cleaned up `_ViewImports.cshtml` to remove unused namespaces.

--- Source/Core/ArtemisBankingPro.Core.Domain/Interfaces/Generic/IGenericRepository.cs
+++ Source/Core/ArtemisBankingPro.Core.Domain/Interfaces/Generic/IGenericRepository.cs
@@ -1,7 +1,11 @@
 ﻿namespace ArtemisBankingPro.Core.Domain.Interfaces.Generic
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<TEntity, TKey>
     {
-
+        Task AddAsync(TEntity entity);
+        Task<int> SaveChangesAsync();
+        Task<TEntity> GetByIdAsync(TKey key);
+        Task<IReadOnlyCollection<TEntity>> GetAllEntitys();
+        Task<bool> UpdateAsync(TEntity entity);
     }
 }
