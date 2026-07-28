798d0912cd34ed9a057cf517833addfc34633474
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-07-28 18:18:49 -0400

    Add LoanError, ChannelPayment, and LoanPayment classes
    Added the `LoandError` static class to define common loan-related
    error messages. Introduced the `ChannelPayment` enum to represent
    payment channels. Added the `LoanPayment` sealed class to model
    loan payment details, including properties for loan ID, installment
    ID, transaction ID, payment timestamp, amount, payment channel,
    and user performing the payment.

---
+++ Source/Core/ArtemisBankingPro.Core.Domain/CodeErrors/LoansErros/LoandError.cs
@@ -0,0 +1,26 @@
+﻿using ArtemisBankingPro.Core.Domain.Common.Errors;
+
+namespace ArtemisBankingPro.Core.Domain.CodeErrors.LoansErros
+{
+    public static class LoandError
+    {
+        public static readonly Error NonExistsLoans
+            = new("Error", "Este cliente no tiene préstamos registrados.");
+
+        public static readonly Error CustomerWithLoanExist =
+            new("Error", "Este cliente ya tiene un préstamo activo asignado.");
+
+        public static readonly Error InvalidTerm
+            = new("Error", "El plazo seleccionado no es válido.");
+
+        public static readonly Error FailedProcessLoan
+            = new("Error", "Ha ocurrido un error imprevisto al procesar el prestamo. Favor intente de nuevo más tarde.");
+
+        public static readonly Error FaildGenerateLoansInstallment
+            = new("Error", "La tabla de amortización de este prestamo no pudo ser creada. Favor intente de nuevo más tarde.");
+
+        public static readonly Error NonExistAccountFirstActive
+            = new("Error", "El cliente no tiene una cuenta de ahorro principal activa para recibir el desembolso ndel préstamo.");
+
+    }
+}
---
+++ Source/Core/ArtemisBankingPro.Core.Domain/Entities/Loans/LoanPayment.cs
@@ -0,0 +1,16 @@
+﻿using ArtemisBankingPro.Core.Domain.Common.Enum;
+
+namespace ArtemisBankingPro.Core.Domain.Entities.Loans
+{
+    public sealed class LoanPayment
+    {
+        public int Id { get; set; }
+        public required int LoandId { get; set; }
+        public required int LoandInstalllmentId { get; set; }
+        public required int TransactionId { get; set; }
+        public required DateTimeOffset PaidAt { get; set; } = DateTimeOffset.UtcNow;
+        public required decimal EffectiveAmount { get; set; }
+        public required ChannelPayment Channel {  get; set; }
+        public required string PerformedByUserId { get; set; }
+    }
+}
