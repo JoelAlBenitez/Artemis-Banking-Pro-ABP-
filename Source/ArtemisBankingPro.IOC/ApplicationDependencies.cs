97bafe0c654ba4f208d7a0060dfabfb930f7275e
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-07-30 10:28:13 -0400

    Create IOC common

---
+++ Source/ArtemisBankingPro.IOC/ApplicationDependencies.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ArtemisBankingPro.IOC
+{
+    public static class ApplicationDependencies
+    {
+        public static IServiceCollection AddApplicationDependecies(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
