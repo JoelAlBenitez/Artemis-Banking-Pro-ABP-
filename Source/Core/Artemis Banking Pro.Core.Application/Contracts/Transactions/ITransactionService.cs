2ed1ba33a70d18c0974dd1b1d2878434b262a826
SebasPeguero <20251172@itla.edu.do>
2026-08-07 13:03:18 -0400

    Añadir soporte para transferencias entre cuentas
    Se añadió el método `ProcessAccountTransferAsync` en la interfaz `ITransactionService` para procesar transferencias entre cuentas.
    
    En `ITransactionsValidationServices`, se modificó el método `ValidateLoanPaymentAsync` para validar transferencias entre cuentas y se añadió el método `ValidateAccountTransferAsync` para validar específicamente estas operaciones.
    
    Se actualizó `AccountTransferViewModel` para incluir la propiedad `AvailableAccounts`, que contiene las cuentas disponibles para realizar transferencias. También se agregó el espacio de nombres necesario para trabajar con cuentas de ahorro.

--- Source/Core/Artemis Banking Pro.Core.Application/Contracts/Transactions/ITransactionService.cs
+++ Source/Core/Artemis Banking Pro.Core.Application/Contracts/Transactions/ITransactionService.cs
@@ -14,5 +14,6 @@ namespace Artemis_Banking_Pro.Core.Application.Contracts.Transactions
         Task<ValidationResult<IReadOnlyCollection<Artemis_Banking_Pro.Core.Application.DTOs.Beneficiaries.BeneficiaryDto>>> GetBeneficiariesAsync(string clientId);
         Task<bool> ProcessDepositAsync(DepositDto depositData);
         Task<TransactionIndicatorsDto> GetCashierDailyIndicatorsAsync(string cashierId);
+        Task<ValidationResult<TransactionResultDto>> ProcessAccountTransferAsync(AccountTransferDto dto, string clientId);
     }
 }
--- Source/Core/Artemis Banking Pro.Core.Application/ViewModels/Transactions/AccountTransferViewModel.cs
+++ Source/Core/Artemis Banking Pro.Core.Application/ViewModels/Transactions/AccountTransferViewModel.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Artemis_Banking_Pro.Core.Application.DTOs.SavingsAccounts;
 
 namespace Artemis_Banking_Pro.Core.Application.ViewModels.Transactions
 {
@@ -13,5 +15,7 @@ namespace Artemis_Banking_Pro.Core.Application.ViewModels.Transactions
         [Required(ErrorMessage = "El monto a transferir es requerido.")]
         [Range(0.01, double.MaxValue, ErrorMessage = "El monto a transferir debe ser mayor que cero.")]
         public decimal Amount { get; set; }
+
+        public IReadOnlyCollection<SavingsAccountDto>? AvailableAccounts { get; set; } = [];
     }
 }
