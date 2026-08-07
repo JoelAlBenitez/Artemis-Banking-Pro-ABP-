dd35afdee56ff83aa2da411692c74f017b59a96b
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-08-07 09:16:12 -0400

    Create Controller

---
+++ Source/Presentation/ArtemisBankingPro.Presentation.WebApp/Controllers/AdminDashboard/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using Artemis_Banking_Pro.Core.Application.Contracts.AdminDashboard;
+using Artemis_Banking_Pro.Core.Application.ViewModels.AdminDashboard;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtemisBankingPro.Presentation.WebApp.Controllers.AdminDashboard
+{
+    [Authorize(Roles = "Administrador")]
+    public class AdminController : Controller
+    {
+        private readonly IAdminDashboardServices _adminDashboardServices;
+        private readonly IMapper _mapper;
+
+        public AdminController(
+         IAdminDashboardServices adminDashboardServices,
+         IMapper mapper   
+         ) { 
+            
+            _adminDashboardServices = adminDashboardServices;
+            _mapper = mapper;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            ///agregar ICurrentUserServices 
+            var data = await _adminDashboardServices.GetDataAdminDashboard();
+            var map = _mapper.Map<AdminDashboardViewModel>(data);
+            return View(map);
+        }
+    }
+}
